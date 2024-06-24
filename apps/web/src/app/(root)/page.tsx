972422075438cec67f059cac9b6332b7d8321cf1
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2024-06-24 21:30:27 +0900

    Setting: Setting clerk for auth

--- apps/web/src/app/(root)/page.tsx
+++ apps/web/src/app/(root)/page.tsx
@@ -1,9 +1,14 @@
+import { UserButton } from "@clerk/nextjs"
+
 const Home = async () => {
 
   return (
+      <>
       <h1>
           Unleash Your Creative Vision with ImagiFix
       </h1>
+          <UserButton afterSignOutUrl="/" />
+      </>
   )
 }
 
