3585f921d11d2152cf6c0d529d25a467803f78b9
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2024-07-06 16:40:44 +0900

    feat: Http Logger Middleware 추가

--- apps/api/src/api/api.module.ts
+++ apps/api/src/api/api.module.ts
@@ -1,14 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { UserModule } from 'src/user/user.module';
 import UserController from './user.controller';
 import { WebhookController } from './webhook.controller';
 import { SvixWebhookGuard } from './guard/svix-webhook.guard';
 import { ImageModule } from 'src/image/image.module';
 import ImageController from './image.controller';
+import { HttpLoggerMiddleware } from './middleware/http-logger.middleware';
 
 @Module({
   imports: [UserModule, ImageModule],
   controllers: [UserController, WebhookController, ImageController],
   providers: [SvixWebhookGuard],
 })
-export class ApiModule {}
+export class ApiModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(HttpLoggerMiddleware).forRoutes('*');
+  }
+}
